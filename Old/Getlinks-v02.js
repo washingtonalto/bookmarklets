14168232c56aa65e402087209237764013cd25b0
Washington Alto <washington.alto@gmail.com>
2021-01-10 12:22:41 +0800

    Initial commit

---
+++ Old/Getlinks-v02.js
@@ -0,0 +1,132 @@
+
+/*
+function checkInternalExternalLink(linkHost,pageHost)
+	Returns "Internal" if linkHost == pageHost else returns "External"
+*/
+function checkInternalExternalLink(linkHost,pageHost) {
+	let strOutput;
+	if (linkHost == pageHost) {
+		strOutput = "Internal";
+	} else {
+		strOutput = "External";
+	}	
+	return strOutput;
+}
+
+/*
+function isValidHttpUrl(strTest)
+	Check if string strTest is a URL and if it is, return true otherwise, return false
+*/
+function isValidHttpUrl(strTest) {
+  let url;
+
+  try {
+    url = new URL(strTest);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
+/*
+function formatHTMLcellvalues(strCellinput)
+	Format HTML table cell values so that string strCellinput is empty string when value is null and string is trimmed
+	Also, if strCellinput is valid URL, then <A> tag is appended
+*/
+function formatHTMLcellvalues(strCellinput) {
+  let strOutput;
+  if (isValidHttpUrl(strCellinput)) {
+    strOutput =
+      "<A HREF='" + strCellinput + "' target='_blank'>" + strCellinput + "</A>";
+  } else {
+    strOutput =
+      strCellinput == null || String(strCellinput).trim().length == 0
+        ? ""
+        : String(strCellinput).trim();
+  }
+  return strOutput;
+}
+
+/*
+function setTableStyle()
+	Sets the HTML table using inline CSS
+*/
+function setTableStyle() {
+  let strOutput = "<STYLE>";
+  strOutput +=
+    "table,th,td { border:1px solid #9E9E9E; border-collapse: collapse  }";
+  strOutput += "th { background: #FFC107; }";
+  strOutput += "</STYLE>";
+  return strOutput;
+}
+
+/*
+function formatPageHeaders(strHeader)
+	Sets and formats the HTML page headers like the <H1>, the page URL and the page title
+*/
+function formatPageHeaders(strHeader) {
+  let strOutput = "<H1>" + strHeader + "</H1>";
+  strOutput += "<STRONG>Page URL: </STRONG>";
+  strOutput +=
+    "<A href='" +
+    location.href +
+    "' target='_blank'>" +
+    location.href +
+    "</A><BR>";
+  strOutput += "<STRONG>Page Title: </STRONG>";
+  strOutput += document.title + "<BR><BR>";
+  return strOutput;
+}
+
+/*
+function formatHTMLTableHeaders()
+	Sets and formats the HTML table headers like the <TH>. Note that arguments is variable and 
+	defines the table header titles to be used
+*/
+function formatHTMLTableHeaders() {
+  let strOutput = "<TABLE>";
+  strOutput += "<TR>";
+  for (let i = 0; i < arguments.length; i++) {
+    strOutput += "<TH>" + arguments[i] + "</TH>";
+  }
+  strOutput += "</TR>";
+  return strOutput;
+}
+
+/*
+function formatHTMLTableRows() 
+	Sets and formats the HTML table row like the <TR> and <TD>. Note that arguments is variable and 
+	defines the table cells to be used
+*/
+function formatHTMLTableRows() {
+  let strOutput = "<TR>";
+  for (let i = 0; i < arguments.length; i++) {
+    strOutput += "<TD>" + formatHTMLcellvalues(arguments[i]) + "</TD>";
+  }
+  strOutput += "</TR>";
+  return strOutput;
+}
+
+var objCollection = document.links;			// define the DOM object as HTML Collections
+var pageHost = location.host				// define the host of the page
+var strHTMLlines = "";						// define the HTML line string 
+strHTMLlines += setTableStyle();
+strHTMLlines += formatPageHeaders("WLA Link Checker v03");
+strHTMLlines += formatHTMLTableHeaders("No", "Link URL", "Link Text","Link Protocol","Internal/External");
+for (let i = 0; i < objCollection.length; i++) {
+  let objItem = objCollection[i];
+  strHTMLlines += formatHTMLTableRows(
+    i + 1,
+    objItem["href"],
+	objItem["innerText"],
+	objItem["protocol"].replace(":",""),
+	checkInternalExternalLink(objItem["host"],pageHost)
+  );
+}
+strHTMLlines += "</TABLE>";
+
+// Open a new tab or window in browser and display the concatenated strings strHTMLlines
+var myWin = window.open();
+myWin.document.writeln(strHTMLlines);
+myWin.document.close();
